2044131e72b698e224ed4a6e1e00458f35d3162a
Jovica Zoric <zoricjovica@gmail.com>
2018-09-23 21:33:43 +0200

    Use department when greeting

--- hellomod.go
+++ hellomod.go
@@ -2,6 +2,6 @@ package hellomod
 
 import "fmt"
 
-func Hello(name string) string {
-  return fmt.Sprintf("Hello %s. Nice to see you!", name)
+func Hello(name string, department string) string {
+  return fmt.Sprintf("Hello %s, from %s!", name, department)
 }
